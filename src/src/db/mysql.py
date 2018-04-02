d7ea7004213496712a3561deedfad90412e577a5
initbar <initbar@protonmail.ch>
2018-04-02 15:05:53 -0400

    add connect

--- src/src/db/mysql.py
+++ src/src/db/mysql.py
@@ -17,6 +17,50 @@
 # https://github.com/initbar/sipd
 
 import MySQLdb as mysql
+import logging
+
+logger = logging.getLogger(__name__)
+
+# MySQL allocator
+#-------------------------------------------------------------------------------
+
+def unsafe_allocate_mysql_client(host, port, username, password, database, table=None):
+    ''' allocate a MySQL client.
+    '''
+    if not all(host, port, username, password, database): return
+    try:
+        mysql_client = MySQLClient(host, port, username, password, database, table)
+        assert mysql_client.connect()
+        logger.info("[mysql] successfully allocated MySQL client.")
+    except Exception as message:
+        logger.error("[mysql] unable to allocate client: '%s'" % message)
+    return locals().get(mysql_client)
+
+def safe_allocate_mysql_client(host, port, username, password, database, table):
+    ''' allocate exception-safe MySQL client.
+    '''
+    def __init__(self, host, port, username, password, database, table):
+        self.host = host
+        self.port = port
+        self.username = username
+        self.password = password
+        self.database = database
+        self.table = table
+
+    def __enter__(self):
+        self._cursor = unsafe_allocate_mysql_client(self.host, self.port,
+                                                    self.username, self.password,
+                                                    self.database, self.table)
+        return self._cursor # connected client.
+
+    def __exit__(self):
+        try: self._cursor.close()
+        except:
+            self._cursor.close() # try again
+            del self._cursor
+
+# MySQL client
+#-------------------------------------------------------------------------------
 
 class MySQLClient(object):
     ''' MySQL client template.
@@ -24,10 +68,23 @@ class MySQLClient(object):
 
     def __init__(self, host, port,
                  username, password,
-                 db, table=None):
+                 database, table=None):
+        # MySQL configuration.
         self.host = host
         self.port = port
         self.username = usernmae
         self.password = password
-        self.db = db
+        self.database = database
         self.table = table
+        # MySQL cursor.
+        self.session = None
+
+    def connect(self):
+        mysql.connect(host=self.host, port=self.port,
+                      user=self.username, passwd=self.password,
+                      db=self.database)
+        return self.session.open()
+
+    def query(self, statement):
+        if not statement: yield
+        yield []
