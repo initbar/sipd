8908e45ec3e67ac4eb4957c99f1d2f66fddfa98c
initbar <initbar@protonmail.ch>
2018-04-18 14:11:28 -0400

    bump version

--- src/__main__.py
+++ src/__main__.py
@@ -36,7 +36,7 @@ from src.sip.server import AsynchronousSIPServer
 from src.tester import run_test_suite
 
 __program__ = 'sipd -- Active recording Session Initiation Protocol Daemon'
-__version__ = '1.2.14'
+__version__ = '1.2.15'
 __license__ = 'GNU GPLv3'
 
 def main():
