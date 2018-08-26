ca3517facff60a180b3f17407f11f9dede9eefa1
initbar <initbar@protonmail.ch>
2018-08-26 12:29:43 -0400

    add __all__

--- src/sip/parser.py
+++ src/sip/parser.py
@@ -39,32 +39,97 @@ from src.sip.methods import SIP_METHODS
 
 logger = logging.getLogger()
 
-__all__ = []
-
+__all__ = [
+    "convert_to_sip_message",
+    "parse_sip_message",
+    "validate_sip_signature",
+]
 
 #
-# string entities
+# entities
 #
-
-
-REGX_SDP = re.compile("^[a-z]{1}=.+$")
 
 COLON = ":"
 COMMA = ","
 CRLF = "\r\n"
 
-
 #
 # SIP
 #
 
+SDP_PATTERN = re.compile("^[a-z]{1}=.+$")
 
 # check if SIP signature exists inside SIP message.
 validate_sip_signature = lambda string: "SIP" in string
 
 
 @lru_cache(maxsize=128, typed=True)
-def convert_to_sip_packet(template: str, datagram: dict) -> str:
+def parse_sip_message(message: str) -> dict:
+    """ convert SIP message into SIP datagram.
+    @message<str> -- SIP message.
+    """
+    if not message:
+        return {}
+
+    # allocate Pythonic object to interface with SIP headers. Originally, the
+    # design was to whitelist known SIP headers into a SIP datagram using
+    # SIPResponse and SIPRequest datagrams. However, since headers will be
+    # unknown (as well as possibly volitile), the design was changed to
+    # dynamically insert any keys extracted from single SIP packet.
+    queue = deque(filter(None, message.replace(CRLF, "\n").split("\n")))
+    datagram = {"sip": {}, "sdp": []}
+    datagram_compressed = datagram.copy()
+
+    # rotate and parse each SIP headers. But first, since set intersection
+    # operation is more computationally expensive than other search operations
+    # - and since we already know that the first header will always indicate
+    # SIP method - parse the method first.
+    header = queue.popleft()
+    try:
+        method = (SIP_METHODS & set(header.split())).pop()
+    except:
+        # TODO: throw exception.
+        return
+    datagram["sip"]["Method"] = [method]
+
+    # for the remaining headers, split by the first occurance of header and
+    # definition CRLF and insert into the dynamic datagram. Since it's possible
+    # to have multiple values for a given key (e.g. Via), temporarily store
+    # the values as list.
+    while queue:
+        header = queue.popleft()
+        try:
+            assert not SDP_PATTERN.match(header)
+            k, v = header.split(COLON, 1)
+            datagram["sip"].setdefault(k, [])
+            datagram["sip"][k].append(v.strip())
+        except:
+            # TODO: print error.
+            pass
+
+    # compress multiple SIP keys into single key.
+    try:
+        for (k, v) in list(datagram["sip"].items()):
+            datagram_compressed["sip"].setdefault(k, [])
+            datagram_compressed["sip"][k] = COMMA.join(v)
+    except:
+        # TODO: print error.
+        pass
+
+    # compress multiple SDP keys into single key.
+    try:
+        for (k, v) in list(datagram["sdp"].items()):
+            datagram_compressed["sdp"].setdefault(k, [])
+            datagram_compressed["sdp"][k] = CRLF.join([sdp for sdp in set(v)])
+    except:
+        # TODO: print error.
+        pass
+
+    return datagram_compressed
+
+
+@lru_cache(maxsize=128, typed=True)
+def convert_to_sip_message(template: str, datagram: dict) -> str:
     """ convert SIP datagram into SIP message.
     @template<str> -- SIP response template.
     @datagram<dict> -- SIP datagram.
@@ -103,68 +168,3 @@ def convert_to_sip_packet(template: str, datagram: dict) -> str:
         message += CRLF
 
     return message
-
-
-@lru_cache(maxsize=128, typed=True)
-def parse_sip_packet(message: str) -> dict:
-    """ convert SIP message into SIP datagram.
-    @message<str> -- SIP message.
-    """
-    if not message:
-        return {}
-
-    # allocate Pythonic object to interface with SIP headers. Originally, the
-    # design was to whitelist known SIP headers into a SIP datagram using
-    # SIPResponse and SIPRequest datagrams. However, since headers will be
-    # unknown (as well as possibly volitile), the design was changed to
-    # dynamically insert any keys extracted from single SIP packet.
-    queue = deque(filter(None, message.replace(CRLF, "\n").split("\n")))
-    datagram = {"sip": {}, "sdp": []}
-    datagram_compressed = datagram.copy()
-
-    # rotate and parse each SIP headers. But first, since set intersection
-    # operation is more computationally expensive than other search operations
-    # - and since we already know that the first header will always indicate
-    # SIP method - parse the method first.
-    header = queue.popleft()
-    try:
-        method = (SIP_METHODS & set(header.split())).pop()
-    except:
-        # TODO: throw exception.
-        return
-    datagram["sip"]["Method"] = [method]
-
-    # for the remaining headers, split by the first occurance of header and
-    # definition CRLF and insert into the dynamic datagram. Since it's possible
-    # to have multiple values for a given key (e.g. Via), temporarily store
-    # the values as list.
-    while queue:
-        header = queue.popleft()
-        try:
-            assert not REGX_SDP.match(header)
-            k, v = header.split(COLON, 1)
-            datagram["sip"].setdefault(k, [])
-            datagram["sip"][k].append(v.strip())
-        except:
-            # TODO: print error.
-            pass
-
-    # compress multiple SIP keys into single key.
-    try:
-        for (k, v) in list(datagram["sip"].items()):
-            datagram_compressed["sip"].setdefault(k, [])
-            datagram_compressed["sip"][k] = COMMA.join(v)
-    except:
-        # TODO: print error.
-        pass
-
-    # compress multiple SDP keys into single key.
-    try:
-        for (k, v) in list(datagram["sdp"].items()):
-            datagram_compressed["sdp"].setdefault(k, [])
-            datagram_compressed["sdp"][k] = CRLF.join([sdp for sdp in set(v)])
-    except:
-        # TODO: print error.
-        pass
-
-    return datagram_compressed
